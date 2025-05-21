5ba401117351f67bf8711e0d2840f99e10e688cb
Nuist666 <yzwangkaicheng@gmail.com>
2025-05-21 15:32:47 +0800

    Site updated: 2025-05-21 15:32:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/20/hello-world/","2025/05/20/archive-old-blogs/"];function toRandomPost(){
+var posts=["2025/05/20/archive-old-blogs/","2025/05/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
