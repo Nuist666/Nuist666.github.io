b850d9a8934ff606f034ded9c96b0e82ebde8377
Nuist666 <yzwangkaicheng@gmail.com>
2025-05-29 23:15:21 +0800

    Site updated: 2025-05-29 23:15:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/20/hello-world/","2025/05/20/archive-old-blogs/","2025/05/26/lrc-maker/"];function toRandomPost(){
+var posts=["2025/05/26/lrc-maker/","2025/05/20/archive-old-blogs/","2025/05/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
